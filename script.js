806e4883ddeb9c0b1ca9ef6a43b12153d3c3c4d9
Santiago Velosa <santiagovelosa@gmail.com>
2021-11-08 14:50:36 -0500

    Add Javascript functionality for menu buttons

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let navButton = document.querySelector(".btn-icon");
-navButton.addEventListener("click", (event) => {
-  event.stopPropagation();
-  event.preventDefault();
-  let menuMobile = document.querySelector(".hamburger");
-  menuMobile.classList.toggle("mobile-menu");
-});
+if (window.innerWidth < 992) {
+  let navButton = document.querySelector(".btn-icon");
+  navButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    console.log('click');
+    event.preventDefault();
+    let menuMobile = document.querySelector(".hamburger");
+    menuMobile.classList.toggle("mobile-menu");
+    console.log(menuMobile.classList, menuMobile.classList.contains('mobile-menu'));
+    let menuButtons = document.querySelectorAll('.actual-links');
+    console.log(menuButtons);
+    menuButtons.forEach(menuButton => menuButton.addEventListener('click', event => {
+      event.stopPropagation();
+      navButton.click();
+    }));
+  });
+}
+
+// let hamburgerElement = document.querySelector('.hamburger');
+// if(hamburgerElement.classList.contains('mobile-menu')) {
+// }
