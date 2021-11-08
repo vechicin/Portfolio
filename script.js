d398a8a602ef3da0aa3aee4b9c2cb8df2598b984
Jorge Vargas <jorgevargasla@gmail.com>
2021-11-08 15:07:10 -0500

    Fix menu for desktop

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 if (window.innerWidth < 992) {
-  let navButton = document.querySelector(".btn-icon");
-  navButton.addEventListener("click", (event) => {
-    event.stopPropagation();
-    console.log('click');
-    event.preventDefault();
-    let menuMobile = document.querySelector(".hamburger");
-    menuMobile.classList.toggle("mobile-menu");
-    console.log(menuMobile.classList, menuMobile.classList.contains('mobile-menu'));
-    let menuButtons = document.querySelectorAll('.actual-links');
-    console.log(menuButtons);
-    menuButtons.forEach(menuButton => menuButton.addEventListener('click', event => {
+  // Elements
+  const navButton = document.querySelector('.btn-icon');
+  const menuMobile = document.querySelector('.hamburger');
+  const menuButtons = document.querySelectorAll('.actual-links');
+
+  // Functions
+  const handleLink = () => {
+    menuButtons.forEach((menuButton) => menuButton.addEventListener('click', (event) => {
       event.stopPropagation();
       navButton.click();
     }));
-  });
-}
+  };
 
-// let hamburgerElement = document.querySelector('.hamburger');
-// if(hamburgerElement.classList.contains('mobile-menu')) {
-// }
+  const handleClick = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    menuMobile.classList.toggle('mobile-menu');
+
+    handleLink();
+  };
+
+  navButton.addEventListener('click', handleClick);
+}
